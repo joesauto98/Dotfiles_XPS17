c43da3a1c4a2e3a04b96393d4b96c90e4337d36e
Joe P <39030788+joesauto98@users.noreply.github.com>
2022-01-02 20:31:23 -0500

    Updated notes in script file

--- setup_script.zsh
+++ setup_script.zsh
@@ -6,5 +6,5 @@ yay -Ss httpie
 yay -Ss bat
 yay -Ss google-chrome
 yay -Ss visual-studio-code-bin
-
+# TO RUN THIS FILE TYPE : ./INSTALL AND THE SCRIPT WILL START WORKING
 # THIS IS JUST A SEARCH TEST FILE --- I DID NOT WANT TO INSTALL JUNK ON MY SYSTEM
