9bb926ac35ea8ed13971da32371c20d28db59288
Joe P <39030788+joesauto98@users.noreply.github.com>
2022-01-02 20:39:11 -0500

    Added firefox to install script

--- setup_script.zsh
+++ setup_script.zsh
@@ -5,7 +5,7 @@ echo "\n<<< Starting  Setup >>>\n"
 yay -Ss httpie
 yay -Ss bat
 yay -Ss exa
-yay -Ss google-chrome
+yay -Ss firefox
 yay -Ss visual-studio-code-bin
 # TO RUN THIS FILE TYPE : ./INSTALL AND THE SCRIPT WILL START WORKING
 # THIS IS JUST A SEARCH TEST FILE --- I DID NOT WANT TO INSTALL JUNK ON MY SYSTEM
