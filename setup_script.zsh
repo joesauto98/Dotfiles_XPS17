bfccb77f3d246038e807bb2cf72303e718294345
Joe P <39030788+joesauto98@users.noreply.github.com>
2022-01-02 20:26:45 -0500

    Adding new files and running with Dotbot

---
+++ setup_script.zsh
@@ -0,0 +1,10 @@
+#!/usr/bin/env zsh
+
+echo "\n<<< Starting  Setup >>>\n"
+
+yay -Ss httpie
+yay -Ss bat
+yay -Ss google-chrome
+yay -Ss visual-studio-code-bin
+
+# THIS IS JUST A SEARCH TEST FILE --- I DID NOT WANT TO INSTALL JUNK ON MY SYSTEM
